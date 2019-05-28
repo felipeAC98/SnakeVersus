e930da8be3646f2047d36471413bf6ea5bef3e2e
Felipe <felipe.alvcor@gmail.com>
2019-05-28 10:14:16 -0300

    Alteracoes no main e melhora na forma de printar a moldura do jogo

--- SnakeVersus.asm
+++ SnakeVersus.asm
@@ -1,5 +1,4 @@
-TITLE Animation example
-;; http://stackoverflow.com/questions/34217344/clear-screen-without-interrupt
+; http://stackoverflow.com/questions/34217344/clear-screen-without-interrupt
 
 INCLUDE Irvine32.inc
 INCLUDE win32.inc
@@ -9,7 +8,7 @@ INCLUDE Macros.inc
 
 COLS = 100; number of columns
 ROWS = 30; number of rows
-CHAR_ATTRIBUTE = 0Fh; bright white foreground
+CHAR_ATTRIBUTE = 0Fh; COR BRANCA
 
 
 .data
@@ -17,101 +16,103 @@ console HANDLE 0
 buffer CHAR_INFO ROWS * COLS DUP(<< ' ' > , CHAR_ATTRIBUTE > )
 bufferSize COORD <COLS, ROWS>
 bufferCoord COORD <0, 0>
-region SMALL_RECT <0, 0, COLS - 1, ROWS - 1>
+region SMALL_RECT <0, 0, COLS , ROWS >
 
 x DWORD 0; current position
 y DWORD 0; of the figure
 
-posjog2x DWORD 99; posicao inicial do jogador
+caracterJog1 WORD '1'
+caracterJog2 WORD '2'
+
+posjog2x DWORD 98; posicao inicial do jogador
 posjog2y DWORD 15;
 
 sumjog2x SDWORD 0; posicao inicial do jogador
 sumjog2y SDWORD 0;
 
-posjog1x DWORD 1; posicao inicial do jogador
+posjog1x DWORD 1; posicao inicial do jogador 1
 posjog1y DWORD 15;
 
-sumjog1x SDWORD 0; posicao inicial do jogador
+sumjog1x SDWORD 0; Somador do jogador 1
 sumjog1y SDWORD 0;
-
-character WORD '0'; filled with this symbol
-
 
 .code
 main PROC
 
-INVOKE GetStdHandle, STD_OUTPUT_HANDLE
-mov console, eax; save console handle
+	INVOKE GetStdHandle, STD_OUTPUT_HANDLE
+	mov console, eax; save console handle
 
-INICIALIZATION :
-mov character, '*'
-call printTelaInicial
-invoke WriteConsoleOutput, console,
-ADDR buffer, bufferSize, bufferCoord, ADDR region
-INVOKE Sleep, 100; delay between frames
-
-ANIMATION :
-call DESENHACABECA1
-call DESENHACABECA2
-mov  eax, 100; sleep, to allow OS to time slice
-call Delay
-call ReadKey
-jz   ANIMATION; no key pressed yet
-mov character, dx
-
-;//################## VERIFICACAO MOVIMENTACAO JOGADOR 1 ##################//
-moveA :
-cmp dx, 'A'
-jne moveD
-call MOVE_A
-jmp ANIMATION
-
-moveD :
-cmp dx, 'D'
-jne moveS
-call MOVE_D
-jmp ANIMATION
-
-moveS :
-cmp dx, 'S'
-jne moveW
-call MOVE_S
-jmp ANIMATION
-
-moveW :
-cmp dx, 'W'
-jne moveI
-call MOVE_W
-jmp ANIMATION
-
-;//################## VERIFICACAO MOVIMENTACAO JOGADOR 2 ##################//
-
-moveI :
-cmp dx, 'I'
-jne moveJ
-call MOVE_I
-jmp ANIMATION
-
-moveJ :
-cmp dx, 'J'
-jne moveL
-call MOVE_J
-jmp ANIMATION
-
-moveL :
-cmp dx, 'L'
-jne moveK
-call MOVE_L
-jmp ANIMATION
-
-moveK :
-cmp dx, 'K'
-jne ANIMATION
-call MOVE_K
-jmp ANIMATION
+	call printMoldura
+	call iniciaMovimentacao
 
 exit
 main ENDP
+
+;//################## FUNCAO DE MOVIMENTACAO E ATUALIZACAO DE TELA DO JOGO ##################
+iniciaMovimentacao PROC
+
+	ANIMATION:
+	call DESENHACABECA1
+	call DESENHACABECA2
+	mov  eax, 100; sleep, to allow OS to time slice
+	call Delay
+	call ReadKey
+	jz   ANIMATION; no key pressed yet
+
+	;//################## VERIFICACAO MOVIMENTACAO JOGADOR 1 ##################//
+	moveA:
+	cmp dx, 'A'
+	jne moveD
+	call MOVE_A
+	jmp ANIMATION
+
+	moveD :
+	cmp dx, 'D'
+	jne moveS
+	call MOVE_D
+	jmp ANIMATION
+
+	moveS :
+	cmp dx, 'S'
+	jne moveW
+	call MOVE_S
+	jmp ANIMATION
+
+	moveW :
+	cmp dx, 'W'
+	jne moveI
+	call MOVE_W
+	jmp ANIMATION
+
+
+
+	;//################## VERIFICACAO MOVIMENTACAO JOGADOR 2 ##################//
+
+	moveI:
+	cmp dx, 'I'
+	jne moveJ
+	call MOVE_I
+	jmp ANIMATION
+
+	moveJ :
+	cmp dx, 'J'
+	jne moveL
+	call MOVE_J
+	jmp ANIMATION
+
+	moveL :
+	cmp dx, 'L'
+	jne moveK
+	call MOVE_L
+	jmp ANIMATION
+
+	moveK :
+	cmp dx, 'K'
+	jne ANIMATION
+	call MOVE_K
+	jmp ANIMATION
+
+iniciaMovimentacao ENDP
 
 ;//################## PROCEDIMENTOS DE CONTROLE DA MOVIMENTACAO JOGADOR 1 ##################//
 
@@ -183,7 +184,9 @@ MOVE_I ENDP
 
 ;//################## PROCEDIMENTO QUE EFETUA A MOVIMENTACAO JOGADOR 1 NA TELA ##################//
 
-DESENHACABECA1 PROC USES eax edx ecx
+DESENHACABECA1 PROC USES eax edx ecx ebx
+
+	mov bx, caracterJog1
 	mov eax, sumjog1x
 	add posjog1x, eax
 	mov eax, sumjog1y
@@ -193,7 +196,7 @@ DESENHACABECA1 PROC USES eax edx ecx
 	mov edx, COLS
 	mul edx; multiplica edx com eax e coloca o resultado em eax.Ele multiplica pois para chegar na linha Y, eh necessario andar no vetor Y*tamanho da linha(numero de colunas)
 	add eax, posjog1x
-	mov buffer[eax * CHAR_INFO].Char, '1'; aqui que eh o desenho da linha da tela
+	mov buffer[eax * CHAR_INFO].Char, bx; aqui que eh o desenho da linha da tela
 
 	invoke WriteConsoleOutput, console,
 	ADDR buffer, bufferSize, bufferCoord, ADDR region
@@ -204,7 +207,9 @@ DESENHACABECA1 ENDP
 
 ;//################## PROCEDIMENTO QUE EFETUA A MOVIMENTACAO JOGADOR 2 NA TELA ##################//
 
-DESENHACABECA2 PROC USES eax edx ecx
+DESENHACABECA2 PROC USES eax edx ecx ebx
+
+mov bx, caracterJog2
 mov eax, sumjog2x
 add posjog2x, eax
 mov eax, sumjog2y
@@ -214,7 +219,7 @@ mov eax, posjog2y
 mov edx, COLS
 mul edx; multiplica edx com eax e coloca o resultado em eax.Ele multiplica pois para chegar na linha Y, eh necessario andar no vetor Y*tamanho da linha(numero de colunas)
 add eax, posjog2x
-mov buffer[eax * CHAR_INFO].Char, '2'; aqui que eh o desenho da linha da tela
+mov buffer[eax * CHAR_INFO].Char, bx; aqui que eh o desenho da linha da tela
 
 invoke WriteConsoleOutput, console,
 ADDR buffer, bufferSize, bufferCoord, ADDR region
@@ -222,67 +227,59 @@ ADDR buffer, bufferSize, bufferCoord, ADDR region
 ret
 DESENHACABECA2 ENDP
 
+;//################## IMPRESSAO MOLDURA JOGO ##################// 
 
+printMoldura PROC USES eax edx ecx
 
+	mov caracterJog1, '*'
 
-;// As próximas funcoes sao responsaveis por desenhar a tela inicial do jogo
+	mov ecx, COLS-1
+	mov sumjog1x, 1
+	mov sumjog1y, 0
+	mov posjog1x, 0
+	mov posjog1y, 0
+molduraSuperior:
+	call DESENHACABECA1
+	loop molduraSuperior
 
-CharToBuffer PROC USES eax edx bufx : DWORD, bufy : DWORD, char : WORD
-mov eax, bufy; bufy = parametro edx = y
-mov edx, COLS
-mul edx; multiplica edx e eax
-add eax, bufx
-mov dx, char
+	mov ecx, COLS -1
+;	mov sumjog1x, 1
+;	mov sumjog1y, 0
+	mov posjog1x, 0
+	mov posjog1y, ROWS -1
 
-cmp bufx, 0
-jne molduraDireita
-mov buffer[eax * CHAR_INFO].Char, dx;
-jmp fimDesenho
+molduraInferior:
+	call DESENHACABECA1
+	loop molduraInferior
+
+	mov ecx, ROWS -1
+	mov sumjog1x, 0
+	mov sumjog1y, 1
+	mov posjog1x, 0
+	mov posjog1y, 0
+molduraEsquerda :
+	call DESENHACABECA1
+	loop molduraEsquerda
+
+	mov ecx, ROWS  -1
+	mov sumjog1x, 0
+	mov sumjog1y, 1
+	mov posjog1x, COLS -1
+	mov posjog1y, 0
 
 molduraDireita :
-cmp bufx, COLS -1
-jne molduraSuperior
-mov buffer[eax * CHAR_INFO].Char, dx;
-jmp fimDesenho
+	call DESENHACABECA1
+	loop molduraDireita
 
-molduraSuperior :
-cmp bufy, 0
-jne molduraInferior
-mov buffer[eax * CHAR_INFO].Char, dx;
-jmp fimDesenho
+	mov sumjog1x, 0
+	mov sumjog1y, 0
+	mov posjog1x, 1
+	mov posjog1y, 15
 
-molduraInferior :
-cmp bufy, ROWS -1
-jne fimDesenho
-mov buffer[eax * CHAR_INFO].Char, dx;
-
-
-fimDesenho:
+	mov caracterJog1, '1'
 ret
-CharToBuffer ENDP
 
-
-;// Segunda funcao responsavel por desenhar tela inicial
-
-printTelaInicial PROC USES eax edx ecx
-mov edx, y
-mov ecx, ROWS
-ONELINE : ; 'Dois loops encadeados aqui'
-	mov eax, x
-
-	push ecx
-	mov ecx, COLS
-
-	ONECHAR : ; 'O outro aqui'
-	INVOKE CharToBuffer, eax, edx, character; edx = y
-	inc eax
-	loop ONECHAR; inner loop prints characters
-	inc edx
-	pop ecx
-	loop ONELINE; outer loop prints lines
-
-	ret
-printTelaInicial ENDP
+printMoldura ENDP
 
 
 
